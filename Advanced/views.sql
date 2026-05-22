4a6dc511273d32253afb4d1d24f6b14238112f0d
Rupsa2112 <your-github-email@gmail.com>
2026-05-22 20:41:59 +0530

    Update views

--- Advanced/views.sql
+++ Advanced/views.sql
@@ -13,7 +13,7 @@ ON c.customer_id = a.customer_id;
 
 SELECT * FROM view_account_summary ORDER BY date_opened;  
 
-DROP VIEW view_loan_status;  
+
 /* Loan Status */
 CREATE VIEW view_loan_status AS 
 SELECT c.customer_id AS "customer_id",
@@ -59,5 +59,50 @@ SELECT * FROM view_transaction_summary;
 
 
 /* Fraud Risk Accounts */
+CREATE VIEW view_tempfraud AS
+SELECT t.account_id AS "account_id",
+       c.name AS "customer_name",
+       t.txn_id AS "transaction_id",
+	   t.amount,
+       AVG(t.amount) OVER(PARTITION BY account_id) AS "avg_amount_per_account"                 
+FROM Customer c
+INNER JOIN Accounts a ON c.customer_id = a.customer_id
+INNER JOIN Transactions t ON a.account_id = t.account_id;
+
+
+CREATE VIEW view_fraud_risk_accounts AS  
+SELECT account_id, 
+       customer_name,
+       transaction_id,
+       amount,
+       avg_amount_per_account, 
+       ROUND((amount / avg_amount_per_account) , 2) AS "times_above_average",
+       CASE 
+			WHEN amount > 10 * avg_amount_per_account THEN 'Critical'
+			WHEN amount > 5 * avg_amount_per_account THEN 'High Risk'
+			WHEN amount > 3 * avg_amount_per_account THEN 'Suspicious'
+	  END AS fraud_flag
+FROM view_tempfraud
+WHERE amount > (3 *  avg_amount_per_account);
+
+SELECT * FROM view_fraud_risk_accounts;
+
+
+/* Card Expiry Alert */
+CREATE VIEW view_card_expiry_alert AS
+SELECT c.customer_id,
+       c.name AS "customer_name",
+       c.phone AS "phone number",
+       cd.card_id AS "card_id",
+       cd.card_type AS "card_type",
+       CONCAT('****-****-****-', RIGHT(card_number, 4)) AS masked_card_number,
+       cd.expiry AS "expiry_date",
+       DATEDIFF(cd.expiry, CURDATE()) AS "days_until_expiry"
+FROM Customer c
+INNER JOIN Accounts a ON c.customer_id = a.customer_id
+INNER JOIN Cards cd ON a.account_id = cd.account_id
+WHERE DATEDIFF(cd.expiry, CURDATE()) < 180;
+
+SELECT * FROM view_card_expiry_alert;
 
 
